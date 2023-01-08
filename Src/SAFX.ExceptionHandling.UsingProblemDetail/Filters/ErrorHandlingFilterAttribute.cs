59c9d331f2831ea73d0eb731440c5fac0305f61d
Seyed Arman Fatemi <S.Arman.Fatemi@outlook.com>
2023-01-08 23:14:29 +0330

    Implement using problem details

---
+++ Src/SAFX.ExceptionHandling.UsingProblemDetail/Filters/ErrorHandlingFilterAttribute.cs
@@ -0,0 +1,23 @@
+﻿namespace SAFX.ExceptionHandling.UsingProblemDetail.Filters;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+
+public class ErrorHandlingFilterAttribute : ExceptionFilterAttribute
+{
+	public override void OnException(ExceptionContext context)
+	{
+		// The exception object is in context.Exception;
+		Exception exception = context.Exception;
+		ProblemDetails problemDetails = new()
+		{
+			Type = "https://tools.ietf.org/html/rfc7231#section-6.6.1",
+			Title = "Internal server error (500) - With custom message (From filter using problem detail)",
+			Status = (int)HttpStatusCode.InternalServerError
+		};
+
+		context.Result = new ObjectResult(problemDetails);
+		context.ExceptionHandled = true;
+	}
+}
