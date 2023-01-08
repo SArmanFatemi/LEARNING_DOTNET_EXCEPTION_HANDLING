362d73f93a8e333b25a8943ab0f1e672a7b12336
Seyed Arman Fatemi <S.Arman.Fatemi@outlook.com>
2023-01-08 23:03:30 +0330

    Implement using filter

---
+++ Src/SAFX.ExceptionHandling.UsingFilters/Filters/ErrorHandlingFilterAttribute.cs
@@ -0,0 +1,20 @@
+﻿namespace SAFX.ExceptionHandling.UsingFilters.Filters;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+
+public class ErrorHandlingFilterAttribute : ExceptionFilterAttribute
+{
+	public override void OnException(ExceptionContext context)
+	{
+		// The exception object is in context.Exception;
+
+		context.Result = new ObjectResult(new { error = "Internal server error (500) - With custom message (From filter)" })
+		{
+			StatusCode = (int)HttpStatusCode.InternalServerError
+		};
+
+		context.ExceptionHandled = true;
+	}
+}
---
+++ Src/SAFX.ExceptionHandling.UsingFilters/Program.cs
@@ -0,0 +1,20 @@
+using SAFX.ExceptionHandling.UsingFilters.Filters;
+
+var builder = WebApplication.CreateBuilder(args);
+{
+	builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
+	builder.Services.AddEndpointsApiExplorer();
+	builder.Services.AddSwaggerGen();
+}
+
+var app = builder.Build();
+{
+	if (app.Environment.IsDevelopment())
+	{
+		app.UseSwagger();
+		app.UseSwaggerUI();
+	}
+	app.MapControllers();
+}
+
+app.Run();
